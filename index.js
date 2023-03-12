7e246eccc96955599de6d1dfd0da30845bcf6494
+ <+>
2023-03-12 19:30:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+var employee = {
+    name :"Enock",
+    streetAddress:"21. Parkhill 400 LA"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+   const newemployee = {...employee}
+   newemployee[key]= value;
+
+   return  newemployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+
+    employee[key]=value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newemployee = {...employee};
+    delete newemployee[key];
+    return newemployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
